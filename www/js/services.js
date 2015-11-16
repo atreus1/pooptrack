a87f16f236c6334838188cd01fbb7e05fc67bbf5
joakim.heldmann <joakim.heldmann@gmail.com>
2015-11-16 03:53:41 -0500

    random int

--- www/js/services.js
+++ www/js/services.js
@@ -3,9 +3,14 @@ angular.module('starter.services', [])
 .service('LoginService', function($q) {
   return {
     loginUser: function() {
-      var d = new Date();
-      var n = d.getTime();
-      var r = Math.floor((Math.random() * n) +1);
+      if(!localStorage['user_id']) {
+        var d = new Date();
+        var n = d.getTime();
+        var r = Math.floor((Math.random() * n) +1);
+        window.localStorage['user_id'] = r;
+      }
+      else
+         window.localStorage['user_id'] = r;
      return r
     }
   }
