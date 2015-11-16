14ef4217b14b4a61392d846816aee436ca27bdb1
joakim.heldmann <joakim.heldmann@gmail.com>
2015-11-16 03:49:27 -0500

    random int

--- www/js/services.js
+++ www/js/services.js
@@ -2,24 +2,11 @@ angular.module('starter.services', [])
 
 .service('LoginService', function($q) {
   return {
-    loginUser: function(name, pw) {
-      var deferred = $q.defer();
-      var promise = deferred.promise;
- 
-      if (name == 'user' && pw == 'secret') {
-        deferred.resolve('Welcome ' + name + '!');
-      } else {
-        deferred.reject('Wrong credentials.');
-      }
-      promise.success = function(fn) {
-        promise.then(fn);
-        return promise;
-      }
-      promise.error = function(fn) {
-        promise.then(null, fn);
-        return promise;
-      }
-      return promise;
+    loginUser: function() {
+      var d = new Date();
+      var n = d.getTime();
+      var r = Math.floor((Math.random() * n) +1);
+     return r
     }
   }
 });
