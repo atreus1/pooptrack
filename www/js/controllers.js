66c50e24f9ff9a29d5ae0949c3908f34fadd0fd4
Jimmy Hedström <jimmy.hedstrom@hotmail.com>
2015-11-25 01:12:30 +0100

    Started working with friends list
    Database now supports password encryption and adding friends

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -108,11 +108,7 @@ angular.module('starter.controllers', ['ngCordova'])
         window.localStorage.setItem("feed", JSON.stringify(data.feed));
 
       } else {
-        console.log(data.error_msg);
-        var welcomePopup = $ionicPopup.alert({
-          title : "Could not get feed",
-          subTitle: data.error_msg
-        });        
+        console.log(data.error_msg);        
       }
     })
     .error(function(data, status, headers, config) {
@@ -155,77 +151,81 @@ angular.module('starter.controllers', ['ngCordova'])
 })
 
 .controller('NearbyCtrl', function($scope, $state, $http, $cordovaGeolocation, $ionicLoading, $compile) {
-	  $scope.$on('$ionicView.beforeEnter', function() {
-	    var sendData = {
-	      'tag': "getFeed",      
-	      'user_id': window.localStorage['user_id']
-	    };
+  $scope.$on('$ionicView.beforeEnter', function() {
+    var sendData = {
+      'tag': "getFeed",      
+      'user_id': window.localStorage['user_id']
+    };
 
-	    $http.post(url, sendData)
-	    .success(function(data, status, headers, config) {
-	      console.log(data);
+    $http.post(url, sendData)
+    .success(function(data, status, headers, config) {
+      console.log(data);
 
-	      if (data.success === 1) {
-	        console.log("getting feed :)");
+      if (data.success === 1) {
+        console.log("getting feed :)");
 
-	        $scope.feed = data.feed;
-	        window.localStorage.setItem("feed", JSON.stringify(data.feed));
+        $scope.feed = data.feed;
+        window.localStorage.setItem("feed", JSON.stringify(data.feed));
 
-	      } else {
-	        console.log(data.error_msg);
-	        var welcomePopup = $ionicPopup.alert({
-	          title : "Could not get feed",
-	          subTitle: data.error_msg
-	        });        
-	      }
-	    })
-	    .error(function(data, status, headers, config) {
-	      console.log('error');
-	    });
+      } else {
+        console.log(data.error_msg);
+        // var welcomePopup = $ionicPopup.alert({
+        //   title : "Could not get feed",
+        //   subTitle: data.error_msg
+        // });        
+      }
+    })
+    .error(function(data, status, headers, config) {
+      console.log('error');
+    });
 
-      var options = {timeout: 10000, enableHighAccuracy: true};
-      $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
+    var options = {timeout: 10000, enableHighAccuracy: true};
+    $cordovaGeolocation.getCurrentPosition(options).then(
+      function(position) {
         var myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         var mapOptions = {
           center: myLatlng,
           zoom: 15,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-      var map = new google.maps.Map(document.getElementById("map"),
-          mapOptions);
+        var map = new google.maps.Map(document.getElementById("map"),mapOptions);
        
-      //Marker + infowindow + angularjs compiled ng-click
-      var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-      var compiled = $compile(contentString)($scope);
+        //Marker + infowindow + angularjs compiled ng-click
+        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+        var compiled = $compile(contentString)($scope);
 
-      var feed = JSON.parse(window.localStorage['feed']);
-      var marker = [];
-      var infowindow = [];
+        if (window.localStorage['feed']) {
+          var feed = JSON.parse(window.localStorage['feed']);
+          var marker = [];
+          var infowindow = [];
 
-      for(var i = 0; i < feed.length; i++) {
-        var tempPos = feed[i].location.split(",");
-        var pos = new google.maps.LatLng(tempPos[0], tempPos[1]);
-        marker[i] = new google.maps.Marker({
-          position: pos,
-          map: map,
-          title: feed[i].name
-        });
+          for(var i = 0; i < feed.length; i++) {
+            var tempPos = feed[i].location.split(",");
+            var pos = new google.maps.LatLng(tempPos[0], tempPos[1]);
+            marker[i] = new google.maps.Marker({
+              position: pos,
+              map: map,
+              title: feed[i].name
+            });
 
-        // infowindow[i] = new google.maps.InfoWindow({
-        // 	position: pos,
-        // 	content: compiled[0]
-        // });
+          // infowindow[i] = new google.maps.InfoWindow({
+          // 	position: pos,
+          // 	content: compiled[0]
+          // });
 
-        // google.maps.event.addListener(marker[i], 'click', function() {
-        //   console.log("fittskalle "+i);
-        //   infowindow[i].open(map,marker[0]);
-        // });
-      }
-     
-      $scope.map = map;
+          // google.maps.event.addListener(marker[i], 'click', function() {
+          //   console.log("fittskalle "+i);
+          //   infowindow[i].open(map,marker[0]);
+          // });
+          }
+          $scope.map = map;
+        } else {
+          console.log("No feed to display on map yet!");
+        }
       }, function(error){
         console.log("Could not get location");
-      });
+      }
+    );
   
   // google.maps.event.addDomListener(window, 'load', initialize);
     $scope.centerOnMe = function() {
@@ -298,7 +298,7 @@ angular.module('starter.controllers', ['ngCordova'])
       return (number.value > $scope.add.rangeValue);
   }
 
-  $scope.add = function(type) {
+  $scope.addToDatabase = function(type) {
     var sendData = {
           'tag': "addPoop",      
           'user_id': window.localStorage['user_id'],
@@ -337,10 +337,60 @@ angular.module('starter.controllers', ['ngCordova'])
   };
 })
 
-.controller('FriendsCtrl', function($scope, $http) {
-// $http.get('http://tolva.nu/poop/getFriends.php').success(function(data) {
-         $scope.users = "data";
-     // });
+.controller('FriendsCtrl', function($scope, $http, $ionicPopup) {
+    var sendData = {
+      'tag': "getAllUsers",
+      'user_id': window.localStorage['user_id'],
+    };
+
+    $http.post(url, sendData)
+    .success(function(data, status, headers, config) {
+      console.log(data);
+
+      if (data.success === 1) {
+        $scope.users = data.users;
+      } else {
+        console.log(data.error_msg);
+        // var welcomePopup = $ionicPopup.alert({
+        //   title : "Error",
+        //   subTitle: data.error_msg
+        // });        
+      }
+    })
+    .error(function(data, status, headers, config) {
+      console.log('error');
+    });
+
+    $scope.addFriend = function(friend) {
+      console.log(friend.user_id);
+      var sendData = {
+        'tag': "addFriend",
+        'user_id': window.localStorage['user_id'],
+        'friend': friend.user_id
+      };
+
+      $http.post(url, sendData)
+      .success(function(data, status, headers, config) {
+        console.log(data);
+
+        if (data.success === 1) {
+          $scope.users = data.users;
+            var friendPopup = $ionicPopup.alert({
+              title : "Makin' connections",
+              subTitle: "You are now friend with \""+sendData["friend"]+"\"!"
+            });          
+        } else {
+          console.log(data.error_msg);
+          // var welcomePopup = $ionicPopup.alert({
+          //   title : "Error",
+          //   subTitle: data.error_msg
+          // });        
+        }
+      })
+      .error(function(data, status, headers, config) {
+        console.log('error');
+      });      
+    }
 })
 
 .controller('ProfileCtrl', function($scope, $http, $ionicPopup, $state) {
