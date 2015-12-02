fa1457829cf3c67bb83338e21b7abc151ccd4bbe
Jimmy Hedström <jimmy.hedstrom@hotmail.com>
2015-12-02 19:34:59 +0100

    Added library

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -5,10 +5,20 @@ module.exports = [
         "id": "cordova-plugin-whitelist.whitelist",
         "pluginId": "cordova-plugin-whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/org.pbernasconi.progressindicator/www/progressIndicator.js",
+        "id": "org.pbernasconi.progressindicator.ProgressIndicator",
+        "pluginId": "org.pbernasconi.progressindicator",
+        "clobbers": [
+            "ProgressIndicator"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.pbernasconi.progressindicator": "1.1.0"
+}
 // BOTTOM OF METADATA
 });
